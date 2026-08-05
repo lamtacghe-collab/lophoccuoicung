7a741dc01050583fbe6d061026aab0cb7468d739
lamtacghe-collab <lamtacghe@gmail.com>
2026-08-05 12:33:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,25 @@ import pandas as pd
 import plotly.express as px
 from datetime import datetime
 
-# --- 1. KẾT NỐI FIREBASE ---
+# --- 1. KẾT NỐI FIREBASE AN TOÀN CHO STREAMLIT CLOUD ---
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_credentials.json")
+    try:
+        config = {
+            "type": st.secrets["type"],
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+        }
+        cred = credentials.Certificate(config)
+    except Exception:
+        cred = credentials.Certificate("firebase_credentials.json")
+        
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://hoc-tap-58166-default-rtdb.asia-southeast1.firebasedatabase.app/'
     })
