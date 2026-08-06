b8cb8329381115c366e71743d39c2440d95d6430
lamtacghe-collab <lamtacghe@gmail.com>
2026-08-06 07:36:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,9 @@ import pandas as pd
 import plotly.express as px
 from datetime import datetime
 
-# --- 1. KẾT NỐI FIREBASE AN TOÀN TỪ SECRETS ---
+# --- 1. KẾT NỐI FIREBASE TRỰC TIẾP TỪ TỆP TRÊN GITHUB ---
 if not firebase_admin._apps:
-    try:
-        config = {
-            "type": st.secrets["type"],
-            "project_id": st.secrets["project_id"],
-            "private_key_id": st.secrets["private_key_id"],
-            "private_key": st.secrets["private_key"],
-            "client_email": st.secrets["client_email"],
-            "client_id": st.secrets["client_id"],
-            "auth_uri": st.secrets["auth_uri"],
-            "token_uri": st.secrets["token_uri"],
-            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-            "universe_domain": "googleapis.com"
-        }
-        cred = credentials.Certificate(config)
-    except Exception:
-        # Dự phòng chạy ở máy local
-        cred = credentials.Certificate("firebase_credentials.json")
-        
+    cred = credentials.Certificate("firebase_credentials.json")
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://hoc-tap-58166-default-rtdb.asia-southeast1.firebasedatabase.app/'
     })
